6cbf90d97908e5c60a20e4700e0e3dbe37df4fbd
Bharat Jogi <59693851+adm-l@users.noreply.github.com>
2026-09-26 12:10:10 +0530

    feat: configure stale job recovery

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -22,51 +22,26 @@ import (
 )
 
 func main() {
-	cfg := config.Load()
-	log, _ := zap.NewProduction()
+	cfg:=config.Load()
+	log,_:=zap.NewProduction()
 	defer log.Sync()
-
 	var db *sql.DB
 	var err error
-	if cfg.ProfilerDatabaseURL != "" {
-		db, err = sql.Open("pgx", cfg.ProfilerDatabaseURL)
-		if err != nil {
-			log.Fatal("open metadata db", zap.Error(err))
-		}
-		db.SetMaxOpenConns(10)
-		db.SetMaxIdleConns(5)
-		db.SetConnMaxLifetime(30 * time.Minute)
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		err = db.PingContext(ctx)
-		cancel()
-		if err != nil {
-			log.Fatal("ping metadata db", zap.Error(err))
-		}
+	if cfg.ProfilerDatabaseURL!=""{
+		db,err=sql.Open("pgx",cfg.ProfilerDatabaseURL)
+		if err!=nil{log.Fatal("open metadata db",zap.Error(err))}
+		db.SetMaxOpenConns(10);db.SetMaxIdleConns(5);db.SetConnMaxLifetime(30*time.Minute)
+		ctx,cancel:=context.WithTimeout(context.Background(),5*time.Second);err=db.PingContext(ctx);cancel()
+		if err!=nil{log.Fatal("ping metadata db",zap.Error(err))}
 	}
-	if db == nil {
-		log.Fatal("DATABASE_URL is required")
-	}
-
-	st := store.NewPostgresStore(db)
-	jobs := worker.NewManager(st, cfg.JobWorkers, log)
+	if db==nil{log.Fatal("DATABASE_URL is required")}
+	st:=store.NewPostgresStore(db)
+	jobs:=worker.NewManager(st,cfg.JobWorkers,log,cfg.JobStaleAfter)
 	jobs.Start()
 	defer jobs.Stop()
-
-	router := api.NewRouter(api.Dependencies{Store: st, Jobs: jobs, Logger: log})
-	srv := &http.Server{Addr: cfg.HTTPAddr, Handler: router, ReadHeaderTimeout: 5 * time.Second, ReadTimeout: 15 * time.Second, WriteTimeout: 60 * time.Second, IdleTimeout: 60 * time.Second}
-
-	go func() {
-		log.Info("server started", zap.String("addr", cfg.HTTPAddr))
-		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatal("http server", zap.Error(err))
-		}
-	}()
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
-	<-sig
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-	_ = srv.Shutdown(ctx)
-	_ = db.Close()
+	router:=api.NewRouter(api.Dependencies{Store:st,Jobs:jobs,Logger:log})
+	srv:=&http.Server{Addr:cfg.HTTPAddr,Handler:router,ReadHeaderTimeout:5*time.Second,ReadTimeout:15*time.Second,WriteTimeout:60*time.Second,IdleTimeout:60*time.Second}
+	go func(){log.Info("server started",zap.String("addr",cfg.HTTPAddr));if err:=srv.ListenAndServe();err!=nil&&!errors.Is(err,http.ErrServerClosed){log.Fatal("http server",zap.Error(err))}}()
+	sig:=make(chan os.Signal,1);signal.Notify(sig,syscall.SIGTERM,syscall.SIGINT);<-sig
+	ctx,cancel:=context.WithTimeout(context.Background(),20*time.Second);defer cancel();_=srv.Shutdown(ctx);_=db.Close()
 }
