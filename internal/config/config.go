44a2e785d98876f0415fc03431dfd5f91adf92bb
Bharat Jogi <59693851+adm-l@users.noreply.github.com>
2026-09-26 12:10:08 +0530

    feat: configure stale job recovery

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,26 +3,25 @@ package config
 import (
 	"os"
 	"strconv"
+	"time"
 )
 
 type Config struct {
 	HTTPAddr, ProfilerDatabaseURL, RedisURL, LogLevel string
-	JobWorkers                                        int
+	JobWorkers int
+	JobStaleAfter time.Duration
 }
 
 func Load() Config {
-	return Config{HTTPAddr: get("HTTP_ADDR", ":8080"), ProfilerDatabaseURL: get("PROFILER_DATABASE_URL", ""), RedisURL: get("REDIS_URL", ""), LogLevel: get("LOG_LEVEL", "info"), JobWorkers: getInt("JOB_WORKERS", 4)}
-}
-func get(k, d string) string {
-	if v := os.Getenv(k); v != "" {
-		return v
+	return Config{
+		HTTPAddr:get("HTTP_ADDR",":8080"),
+		ProfilerDatabaseURL:get("PROFILER_DATABASE_URL",""),
+		RedisURL:get("REDIS_URL",""),
+		LogLevel:get("LOG_LEVEL","info"),
+		JobWorkers:getInt("JOB_WORKERS",4),
+		JobStaleAfter:getDuration("JOB_STALE_AFTER",30*time.Minute),
 	}
-	return d
 }
-func getInt(k string, d int) int {
-	v, e := strconv.Atoi(get(k, ""))
-	if e != nil || v < 1 {
-		return d
-	}
-	return v
-}
+func get(k,d string)string{if v:=os.Getenv(k);v!=""{return v};return d}
+func getInt(k string,d int)int{v,e:=strconv.Atoi(get(k,""));if e!=nil||v<1{return d};return v}
+func getDuration(k string,d time.Duration)time.Duration{v,e:=time.ParseDuration(get(k,""));if e!=nil||v<=0{return d};return v}
